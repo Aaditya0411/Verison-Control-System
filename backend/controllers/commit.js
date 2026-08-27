2e4de0cc1251f5c240cf31ecec492389c32e403e
AadiCode04 <aadigithub61@gmail.com>
2026-08-27 12:51:39 +0530

    changes added

--- backend/controllers/commit.js
+++ backend/controllers/commit.js
@@ -1,7 +1,33 @@
+const fs = require('fs').promises;
+const { log } = require('console');
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-async function commitRepo() {
-console.log("Mesaage is Commited");
-    
+
+async function commitRepo(message) {
+    const repoPath = path.resolve(process.cwd(), "mygit");
+    const stagedPath = path.join(repoPath, "staging");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        const commitId= uuidv4();
+        const commitDir = path.join(commitsPath , commitId);
+        await fs.mkdir(commitDir , {recursive : true});
+
+        const files = await fs.readdir(stagedPath);
+        for( const file of files){
+            await fs.copyFile(path.join(stagedPath , file) , path.join(commitDir , file));
+        }
+
+        await fs.writeFile(path.join(commitDir , "commit.json") ,  JSON.stringify({message , date :new Date().toISOString()}));
+
+        console.log(`Commit ${commitId} created with message ${message}`);
+        
+
+    } catch (err) {
+        console.error("Error committing files : "  , err);
+        
+    }
 }
 
-module.exports = {commitRepo};
+module.exports = { commitRepo };
