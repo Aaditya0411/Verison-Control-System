3ad5c890c6657526956ac49c12fafb436a5a1dea
AadiCode04 <aadigithub61@gmail.com>
2026-08-31 13:17:50 +0530

    Frontend Component Adding routes and Auth

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import { useNavigate, useRoutes } from 'react-router-dom';
+
+//pages List
+import Dashboard from './components/dashboard/Dashboard';
+import Profile from './components/user/Profile';
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+
+// auth context
+import { useAuth } from './authContent';
+
+const ProjectRoutes = () => {
+    const { currentUser, setCurrentUser } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const userIdFromStorage = localStorage.getItem("userId");
+
+        if (userIdFromStorage && !currentUser) {
+            setCurrentUser(userIdFromStorage);
+
+
+        }
+
+        if (!userIdFromStorage && !["/auth", "/singup"].includes(window.location.pathname)) {
+            navigate("/auth");
+        }
+
+        if (userIdFromStorage && window.location.pathname == '/auth') {
+            navigate("/");
+        }
+    }, [currentUser, navigate, setCurrentUser]
+    );
+    let element = useRoutes([
+        {
+            path : "/",
+            element : <Dashboard/>
+        },
+        {
+            path : "/auth",
+            element : <Login/>
+        },
+        {
+            path : "/signup",
+            element : <Signup/>
+        },
+        {
+            path : "/profile",
+            element : <Profile/>
+        }
+    ]);
+
+
+    return element;
+}
+
+export default ProjectRoutes;
+
+
